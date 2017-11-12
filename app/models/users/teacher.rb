dc3ae14534553905674ff5dca8c2432c59bb620a
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-11-12 17:47:47 -0500

    run annotate

--- app/models/users/teacher.rb
+++ app/models/users/teacher.rb
@@ -2,15 +2,16 @@
 #
 # Table name: users
 #
-#  id              :integer          not null, primary key
-#  first_name      :string
-#  last_name       :string
-#  username        :string
-#  email           :string
-#  password_digest :string
-#  created_at      :datetime         not null
-#  updated_at      :datetime         not null
-#  type            :string
+#  id                    :integer          not null, primary key
+#  first_name            :string
+#  last_name             :string
+#  username              :string
+#  email                 :string
+#  password_digest       :string
+#  created_at            :datetime         not null
+#  updated_at            :datetime         not null
+#  type                  :string
+#  password_confirmation :string
 #
 
 class Teacher < User
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,19 @@
 # == Route Map
 #
-#      Prefix Verb URI Pattern             Controller#Action
-#        root GET  /                       welcome#index
-#    teachers POST /teachers(.:format)     teachers#create
-# new_teacher GET  /teachers/new(.:format) teachers#new
-#     teacher GET  /teachers/:id(.:format) teachers#show
-#    students POST /students(.:format)     students#create
-# new_student GET  /students/new(.:format) students#new
-#     student GET  /students/:id(.:format) students#show
+#             Prefix Verb URI Pattern                                 Controller#Action
+#               root GET  /                                           welcome#index
+#    teacher_lessons GET  /teachers/:teacher_id/lessons(.:format)     lessons#index
+#                    POST /teachers/:teacher_id/lessons(.:format)     lessons#create
+# new_teacher_lesson GET  /teachers/:teacher_id/lessons/new(.:format) lessons#new
+#     teacher_lesson GET  /teachers/:teacher_id/lessons/:id(.:format) lessons#show
+#           teachers POST /teachers(.:format)                         teachers#create
+#        new_teacher GET  /teachers/new(.:format)                     teachers#new
+#            teacher GET  /teachers/:id(.:format)                     teachers#show
+#           students POST /students(.:format)                         students#create
+#        new_student GET  /students/new(.:format)                     students#new
+#            student GET  /students/:id(.:format)                     students#show
+#            lessons GET  /lessons(.:format)                          lessons#index
+#                    POST /lessons(.:format)                          lessons#create
 # 
 
 Rails.application.routes.draw do
