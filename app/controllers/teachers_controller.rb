45f617d1a40c15ee5a33f2aaff9294c571be4157
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-11-12 15:59:38 -0500

    add logic to create new Teacher from signup

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -4,8 +4,23 @@ class TeachersController < ApplicationController
   end
 
   def create
+    @teacher = Teacher.new(teacher_params)
+
+    if @teacher.save
+      session[:user_id] = @teacher.id
+
+      redirect_to teacher_path(@teacher)
+    else
+      render :new
+    end
   end
 
   def show
+  end
+
+  private 
+
+  def teacher_params
+    params.require(:teacher).permit(:first_name, :last_name, :username, :email, :password, :password_confirmation)
   end
 end
