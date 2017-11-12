9128fd52eb8b78152021528ace3c266c05742bed
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-11-12 17:06:27 -0500

    add stubbed out code to help distinguish between a request for all lessons or a specific teacher's. add #set_teacher and #lessons_params

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,6 +1,12 @@
 class LessonsController < ApplicationController
+  before_action :set_teacher, only: %i[new]
+
   def index
-    @lessons = Lesson.all
+    if params[:user_id]
+      #add code to retrieve teacher's lessons here
+    else
+      @lessons = Lesson.all
+    end
   end
 
   def new
@@ -8,8 +14,19 @@ class LessonsController < ApplicationController
   end
 
   def create
+    
   end
 
   def show
+  end
+
+  private
+
+  def set_teacher
+    @teacher = Teacher.find_by(id: params[:teacher_id])
+  end
+
+  def lessons_params
+    params.require(:lesson).permit(:title, :description, :location, :teacher_id)
   end
 end
