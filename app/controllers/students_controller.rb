11bea4590f5fae55d5064de8eb4ea9fdcfba5a2b
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-11-12 15:28:19 -0500

    create Students- and TeachersController. add #new, #create, #show to each

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,10 @@
+class StudentsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+end
---
+++ app/controllers/teachers_controller.rb
@@ -0,0 +1,10 @@
+class TeachersController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+end
