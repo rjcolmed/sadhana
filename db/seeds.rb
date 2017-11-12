48716072699d9ba765bdc655bcd3b72275e5fb1b
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-11-12 14:41:04 -0500

    add a few seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,52 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+student1 = Student.create(
+  first_name: "Scully", 
+  last_name: "Colon", 
+  email: "scully@email.com",
+  password: "111"
+)
+
+student2 = Student.create(
+  first_name: "Ricardo", 
+  last_name: "Colon", 
+  email: "ricardo@email.com",
+  password: "111"
+)
+
+teacher1 = Teacher.create(
+  first_name: "Caroline", 
+  last_name: "McEneaney", 
+  email: "caro@email.com",
+  password: "111"
+)
+
+teacher2 = Teacher.create(
+  first_name: "Jomney", 
+  last_name: "Bimbert", 
+  email: "jomney@email.com",
+  password: "111"
+)
+
+lesson1 = Lesson.create(
+  title: "Vinyasa for Beginners", 
+  description: "This class is for beginners.",
+  location: "123 Broadway",
+  teacher_id: teacher1.id
+)
+
+lesson2 = Lesson.create(
+  title: "Advanced Vinyasa", 
+  description: "This class is for experienced practioners.",
+  location: "8 2nd Ave.",
+  teacher_id: teacher2.id
+)
+
+lesson3 = Lesson.create(
+  title: "Sanskrit for Beginners", 
+  description: "We'll learn the basics.",
+  location: "123 Broadway",
+  teacher_id: teacher1.id
+)
+
