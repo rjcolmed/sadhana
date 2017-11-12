e122a664d66b22be412f084b94311307d9213590
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-11-12 15:04:23 -0500

    run annotate

--- app/models/student_lesson.rb
+++ app/models/student_lesson.rb
@@ -1,3 +1,12 @@
+# == Schema Information
+#
+# Table name: student_lessons
+#
+#  id         :integer          not null, primary key
+#  student_id :integer
+#  lesson_id  :integer
+#
+
 class StudentLesson < ApplicationRecord
   belongs_to :student
   belongs_to :lesson
