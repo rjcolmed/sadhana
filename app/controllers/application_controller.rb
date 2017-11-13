1f567a2971f3d24d775694c776b37cec8b3137c7
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-11-13 09:47:36 -0500

    add #current_user, #is_logged_in?, #is_teacher? pass each to helper_method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :current_user, :is_logged_in?, :is_teacher?
+
+  private
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def is_logged_in?
+    !!current_user
+  end
+
+  def is_teacher?
+    current_user.type == 'Teacher'
+  end
 end
