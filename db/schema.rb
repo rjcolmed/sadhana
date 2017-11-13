8a3a691c7c07438f9b8562df31ee01673d0c789f
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-11-13 12:56:37 -0500

    migrate lesson_tags and tags tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171112203822) do
+ActiveRecord::Schema.define(version: 20171113174945) do
+
+  create_table "lesson_tags", force: :cascade do |t|
+    t.integer "lesson_id"
+    t.integer "tag_id"
+  end
 
   create_table "lessons", force: :cascade do |t|
     t.string "title"
@@ -24,6 +29,12 @@ ActiveRecord::Schema.define(version: 20171112203822) do
   create_table "student_lessons", force: :cascade do |t|
     t.integer "student_id"
     t.integer "lesson_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
