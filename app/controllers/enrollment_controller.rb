8b6196c006d612b64a31f175d7f8b74c2d2d9b53
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-11-13 16:06:56 -0500

    create #set_student, refactor logic in #create, add #destory, allowing student to remove self from lessons students collection

--- app/controllers/enrollment_controller.rb
+++ app/controllers/enrollment_controller.rb
@@ -1,13 +1,30 @@
 class EnrollmentController < ApplicationController
+  before_action :set_lesson, only: %i[create destroy]
+  before_action :set_student, only: %i[create destroy]
+
   def create
     if params[:lesson_id]
-      lesson = Lesson.find_by(id: params[:lesson_id])
-      student = current_user
-      lesson.students << student
+      @lesson.students << @student
 
-      redirect_to student_lessons_path(student)
+      redirect_to student_lessons_path(@student)
     else
-      redirect_to student_path(current_user)
+      redirect_to student_path(@student)
     end
+  end
+
+  def destroy
+    @lesson.students.delete(@student)
+
+    redirect_to student_lessons_path(@student)
+  end
+
+  private
+
+  def set_lesson
+    @lesson = Lesson.find_by(id: params[:lesson_id])
+  end
+
+  def set_student
+    @student = Student.find_by(id: current_user.id)
   end
 end
