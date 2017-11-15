e7b03a05bdac20a3cd1837bdda6dbf6663513291
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-11-15 11:00:04 -0500

    create #omniauth?, #create_by_omniauth, and #find_by_omniauth. add #omni_auth? to validations

--- app/models/users/user.rb
+++ app/models/users/user.rb
@@ -17,9 +17,26 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :first_name, :last_name, :email, :username, presence: true
+  validates :first_name, :last_name, :email, :username, presence: true, unless: :omniauth?
 
   def full_name
     "#{first_name} #{last_name}"
+  end
+
+  def self.create_by_omniauth(auth_hash)
+    User.new.tap do |user|
+      user.password = SecureRandom.hex
+      user.password_confirmation = user.password
+      user.image = auth_hash['info']['image']
+      user.uid = auth_hash['uid']
+    end
+  end
+
+  def self.find_by_omniauth(auth_hash)
+    where(email: auth_hash['info']['email']).first
+  end
+
+  def omniauth?
+    !!uid
   end
 end
