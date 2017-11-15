a42b5d3fbcad27604eb599076f9834195dfbf94f
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-11-15 11:20:50 -0500

    remove from sessions controller the unecessary conditional checking for auth_hash

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,22 +3,14 @@ class SessionsController < ApplicationController
   end
 
   def create
-    if auth_hash = request.env['omniauth.auth']
-      
-      # @user = User.find_or_create_by_omniauth(auth_hash)
-      # session[:user_id] = @user.id
+    @user = User.find_by(username: sessions_params[:username])
 
-      # is_teacher? ? (redirect_to teacher_path(@user)) : (redirect_to student_path(@user))
+    if @user && @user.authenticate(sessions_params[:password])
+      session[:user_id] = @user.id
+
+      is_teacher? ? (redirect_to teacher_path(@user)) : (redirect_to student_path(@user))
     else
-      @user = User.find_by(username: sessions_params[:username])
-
-      if @user && @user.authenticate(sessions_params[:password])
-        session[:user_id] = @user.id
-
-        is_teacher? ? (redirect_to teacher_path(@user)) : (redirect_to student_path(@user))
-      else
-        render :new
-      end
+      render :new
     end
   end
 
