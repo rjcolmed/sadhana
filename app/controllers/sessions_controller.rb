62481eb8f366b220c3bb5f2c9b85ad92b00ebc5e
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-11-15 11:01:31 -0500

    add conditional to #create checking for auth_hash from facebook

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,14 +3,22 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(username: sessions_params[:username])
+    if auth_hash = request.env['omniauth.auth']
+      
+      # @user = User.find_or_create_by_omniauth(auth_hash)
+      # session[:user_id] = @user.id
 
-    if @user && @user.authenticate(sessions_params[:password])
-      session[:user_id] = @user.id
-
-      is_teacher? ? (redirect_to teacher_path(@user)) : (redirect_to student_path(@user))
+      # is_teacher? ? (redirect_to teacher_path(@user)) : (redirect_to student_path(@user))
     else
-      render :new
+      @user = User.find_by(username: sessions_params[:username])
+
+      if @user && @user.authenticate(sessions_params[:password])
+        session[:user_id] = @user.id
+
+        is_teacher? ? (redirect_to teacher_path(@user)) : (redirect_to student_path(@user))
+      else
+        render :new
+      end
     end
   end
 
