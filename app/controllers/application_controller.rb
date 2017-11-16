6850dae04e59925a04890215c8a29f4138112321
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-11-15 19:02:27 -0500

    move all helper functions that were defined in ApplicationController to application helper file and include  that file in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,30 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :current_user, :is_logged_in?, :is_teacher?, :is_student?, :is_lesson_owner?, :owns_show_page?
+  include ApplicationHelper
 
-  private
 
-  def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
-  end
 
-  def is_logged_in?
-    !!current_user
-  end
-
-  def is_teacher?
-    current_user.type == 'Teacher' if current_user
-  end
-
-  def is_student?
-    current_user.type == 'Student' if current_user
-  end
-
-  def is_lesson_owner?
-    current_user == @lesson.teacher
-  end
-
-  def owns_show_page?
-    current_user == @teacher
-  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,26 @@
 module ApplicationHelper
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def is_logged_in?
+    !!current_user
+  end
+
+  def is_teacher?
+    current_user.type == 'Teacher' if current_user
+  end
+
+  def is_student?
+    current_user.type == 'Student' if current_user
+  end
+
+  def is_lesson_owner?
+    current_user == @lesson.teacher
+  end
+
+  def owns_show_page?
+    current_user == @teacher
+  end
 end
