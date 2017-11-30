a9e93dc23997ca2369c4206f08b0c57d09d71401
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-11-30 11:54:57 -0500

    migrate comments table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171115185025) do
+ActiveRecord::Schema.define(version: 20171130163718) do
+
+  create_table "comments", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "lesson_tags", force: :cascade do |t|
     t.integer "lesson_id"
