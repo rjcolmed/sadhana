67e5840d9fb2295294b0a37483a8642caa5d30e3
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-11-30 11:58:18 -0500

    generate addBodyToComments migration and migrate to add body column to comments table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171130165440) do
+ActiveRecord::Schema.define(version: 20171130165716) do
 
   create_table "comment_lessons", force: :cascade do |t|
     t.integer "lesson_id"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20171130165440) do
   create_table "comments", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "body"
   end
 
   create_table "lesson_tags", force: :cascade do |t|
