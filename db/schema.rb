8b147b5315baa684338d9a9db23fc66a289da62a
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-11-30 12:01:18 -0500

    add student_id column to comments table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171130165716) do
+ActiveRecord::Schema.define(version: 20171130165949) do
 
   create_table "comment_lessons", force: :cascade do |t|
     t.integer "lesson_id"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20171130165716) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "body"
+    t.integer "student_id"
   end
 
   create_table "lesson_tags", force: :cascade do |t|
