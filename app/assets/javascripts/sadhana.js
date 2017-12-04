50ffda53c7c3a718c35fb6aa4892158496c8be45
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-12-04 10:32:22 -0500

    add logic to AJAX request callback to render received lessons JSON into an unordered list

--- app/assets/javascripts/sadhana.js
+++ app/assets/javascripts/sadhana.js
@@ -4,10 +4,22 @@ $(() => {
 
 function setListeners() {
   //get individual teacher's lessons
-  $('.js-lessons').click(() => {
+  $('.js-lessons').click((event) => {
+    event.preventDefault();
+
     let id = $('.js-lessons').attr('data-id');
-    $.get(`/teachers/${id}/lessons.json`, (res) => {
-      console.log(res);
+    let lessonsHTML = '<ul>'
+
+    $.get(`/teachers/${id}/lessons.json`, (lessons) => {
+      for(let i = 0; i < lessons.length; i++) {
+        lessonsHTML += `<li>${lessons[i].title}</li>`
+        console.log(lessonsHTML);
+      }
+
+      lessonsHTML += '</ul>'
+
+      console.log(lessonsHTML);
+      $('#lessonInfo').html(lessonsHTML);
     });
   });
 
