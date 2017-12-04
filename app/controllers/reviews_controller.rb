efe839b64566e183a4d0c5b550e2aa7a86d20e44
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-12-04 18:00:41 -0500

    add #index to reviews controller and conditional rendering of reviews (depending on whether params includes teacher_id). also add respond_to macro handling HTML and JSON requests

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,19 @@
 class ReviewsController < ApplicationController
+  before_action :set_teacher, only: %i[index]
+
+  def index
+    if params[:teacher_id]
+      @reviews = set_teacher.reviews
+    else
+      @reviews = Review.all
+    end
+
+    respond_to do |format|
+      format.html { render :index }
+      format.json { render json: @reviews }
+    end
+  end
+
   def create
     @review = Review.create(review_params)
     render json: @review, status: 201
@@ -17,5 +32,9 @@ class ReviewsController < ApplicationController
 
   def review_params
     params.require(:review).permit(:body, :student_id, :teacher_id)
+  end
+
+  def set_teacher
+    @teacher = Teacher.find_by(id: params[:teacher_id])
   end
 end
