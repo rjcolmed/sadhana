2798c3a46948a3cbf06ef86030b246eb1cdaf6b8
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-12-04 18:01:44 -0500

    add click listener that fires get request to reviews controller and formats in HTML the received JSON objects representing student reviews of teacher

--- app/assets/javascripts/sadhana.js
+++ app/assets/javascripts/sadhana.js
@@ -71,6 +71,38 @@ function setListeners() {
     });
   }); 
 
+  $('.js-reviews').click((event) => {
+    event.preventDefault();
+
+    let teacherId = $('.js-reviews').attr('data-id');
+    let teacherReviewsHTML = '<h3 class="ui dividing header">Reviews</h3>'
+    
+    $.get(`/teachers/${teacherId}/reviews.json`, (reviews) => {
+      
+      for(let i = 0; i < reviews.length; i++) {
+        teacherReviewsHTML += `
+        <div class="comment">
+        <a class="avatar">
+          <img src="${reviews[i].student.image}">
+        </a>
+        <div class="content">
+          <a class="author">${reviews[i].student.first_name} ${reviews[i].student.last_name}</a>
+          <div class="metadata">
+            <span class="date">${reviews[i].created_at}</span>
+          </div>
+          <div class="text">
+            ${reviews[i].body}
+          </div>
+        </div>
+      </div>`
+      }
+
+      $('.ui.comments').html(teacherReviewsHTML);
+
+    }); 
+   
+  });
+
 }
 
 
