1de01289ea51fc61c15d78bcefc8b90438cceeb5
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-12-04 15:49:43 -0500

    add #show to reviews controller and respon_to macro to handle html and json requests

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,6 +4,15 @@ class ReviewsController < ApplicationController
     render json: @review, status: 201
   end
 
+  def show
+    @review = Review.find_by(id: params[:id])
+
+    respond_to do |format|
+      format.html { render :show }
+      format.json { render json: @review }
+    end
+  end
+
   private
 
   def review_params
