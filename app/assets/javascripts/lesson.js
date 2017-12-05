7dab6e00a538ce5af3a0a99e8e78b496b3be75a0
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-12-05 17:43:53 -0500

    remove next lesson button's AJAX functionality from sadhana.js and move into lesson.js

--- app/assets/javascripts/lesson.js
+++ app/assets/javascripts/lesson.js
@@ -1,16 +1,65 @@
 class Lesson {
   constructor(json) {
-    this.id = json.id;
-    this.title = json.title;
-    this.time = json.time;
-    this.description = json.description;
-    this.teacher = json.teacher;
-    this.tags = json.tags;
-  }
+     this.id = json.id;
+     this.title = json.title;
+     this.location = json.location;
+     this.time = json.time;
+     this.description = json.description;
+     this.teacher = json.teacher;
+     this.tags = json.tags;
+   }
 
-  teacherName() {
-    return `${this.teacher.first_name} ${this.teacher.last_name}`
-  }
+   teacherName() {
+     return `${this.teacher.first_name} ${this.teacher.last_name}`
+   }
+ }
+ 
+ Lesson.success = (json) => {
+   //this is where I should replace pieces of the page. need to preserve rails's stuff when I dynamically render stuff
+   let lesson = new Lesson(json);
+  //  let lessonHTML = lesson.renderHTML(); //expand on this to render the pieces of html you need
+ 
+   $('#lessonTitle').text(lesson.title);
+   $('#lessonDescription').text(lesson.description);
+   $('#lessonLocation').text(lesson.location);
+   $('#lessonTime').text(lesson.time);
+   $('#lessonTeacherImage').attr('src', lesson.teacher.image);
 
-  
-}
+   let teacherLinkHTML = Lesson.teacherLinkTemplate(lesson);
+   $('#lessonTeacherName').html(teacherLinkHTML);
+
+   let tagsHTML = Lesson.tagsTemplate(lesson);
+   $('#lessonTags').html(tagsHTML);
+
+   $(".js-next").attr('data-id', lesson.id);
+ }
+ 
+ Lesson.error = (err) => {
+   console.log(err);
+ }
+ 
+ Lesson.nextButtonClickListener = (event) => {
+    event.preventDefault();
+    let nextLessonId = parseInt($('.js-next').attr('data-id')) + 1;
+   
+    $.get(`/lessons/${nextLessonId}.json`)
+      .done(Lesson.success)
+      .fail(Lesson.error);
+ }
+ 
+ Lesson.bindNextButtonClickListener = () => {
+   $('.js-next').click(Lesson.nextButtonClickListener);
+ }
+ 
+ Lesson.ready = () => {
+   Lesson.teacherLinkSource = $('#teacher-link-template').html();
+   Lesson.teacherLinkTemplate = Handlebars.compile(Lesson.teacherLinkSource);
+   Lesson.tagsSource = $('#tags-template').html();
+   Lesson.tagsTemplate = Handlebars.compile(Lesson.tagsSource);
+   Lesson.bindNextButtonClickListener();
+ }
+ 
+ $(() => {
+   Lesson.ready();
+ });
+
--- app/assets/javascripts/sadhana.js
+++ app/assets/javascripts/sadhana.js
@@ -19,38 +19,7 @@ function setListeners() {
       $('.ui.comments').html(lessonsHTML);
     });
   });
-
-  //get next teacher lesson
-  $('.js-next').click((event) => {
-    event.preventDefault();
-
-    let nextLessonId = parseInt($('.js-next').attr('data-id')) + 1;
-
-    $.get(`/lessons/${nextLessonId}.json`, lesson => {
-
-      $('#lessonTitle').text(lesson.title);
-      $('#lessonDescription').text(lesson.description);
-      $('#lessonLocation').text(lesson.location);
-      $('#lessonTime').text(lesson.time);
-      $('#lessonTeacherImage').attr('src', lesson.teacher.image);
-
-      let lessonTeacherNameHTML = `<a href="/teachers/${lesson.teacher.id}">
-                                ${lesson.teacher.first_name} ${lesson.teacher.last_name}
-                              </a>`
-      $('#lessonTeacherName').html(lessonTeacherNameHTML);
-
-      let lessonTagsHTML = '';
-      for(let i = 0; i < lesson.tags.length; i++) {
-        lessonTagsHTML += `<div class="ui teal tag label"><a href="/tags/${lesson.tags[i].id}">
-                            ${lesson.tags[i].name}</a>
-                          </div>`
-      }
-      $('#lessonTags').html(lessonTagsHTML);
-      
-      $(".js-next").attr('data-id', lesson.id);
-    });
-  });
-
+  
   $('#reviewForm').submit(function(event) {
     event.preventDefault();
 
